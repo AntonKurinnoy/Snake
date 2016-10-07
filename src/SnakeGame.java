369dea69ab2ab344e8d720913436d84105422518
Anton Kurinnoy <anton.kurinnoy@gmail.com>
2016-10-08 01:15:12 +0300

    base commit

---
+++ src/SnakeGame.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class SnakeGame extends JPanel {
+
+    public static int WIDTH = 600;
+    public static int HEIGHT = 550;
+    public static String NAME = "MY GAME!!!";
+
+    public SnakeGame() {
+    }
+
+    public void paint(Graphics g){
+        g.setColor(color(50,100,100));
+        g.fillRect(0,0,WIDTH,HEIGHT);
+    }
+
+    public Color color(int red, int green, int blue){
+        return new Color(red, green, blue);
+    }
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame(SnakeGame.NAME);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(WIDTH,HEIGHT);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.add(new SnakeGame());
+        frame.setVisible(true);
+
+    }
+}
