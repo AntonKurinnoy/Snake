31cdd1b3fc11a88e16f8c48537d8bb5dd6a1a4ed
Anton Kurinnoy <anton.kurinnoy@gmail.com>
2016-10-08 03:25:54 +0300

    lession 2 point

---
+++ src/Point.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+
+public class Point {
+    public int x;
+    public int y;
+    public String sym;
+
+    public Point(int x, int y, String sym) {
+        this.x = x;
+        this.y = y;
+        this.sym = sym;
+    }
+
+    public Color setColor(int red, int green, int blue){
+        return new Color(red,green,blue);
+    }
+
+    public void draw(Graphics g){
+        g.drawString(this.sym,this.x,this.y);
+    }
+}
--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -15,7 +15,9 @@ public class SnakeGame extends JPanel {
         g.fillRect(0,0,WIDTH,HEIGHT);
 
         g.setColor(color(0,250,250));
-        g.drawString("*",100,100);
+        Point p1 = new Point(100,100,"9jh");
+        p1.setColor(0,250,250);
+        p1.draw(g);
     }
 
     public Color color(int red, int green, int blue){
